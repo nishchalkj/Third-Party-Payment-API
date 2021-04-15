95e95a857d69476cc25b1d6b2128946e443dafcf
Nishchalkj <nishchaln61@gmail.com>
2021-04-15 22:00:30 +0530

    Update TestUtils class

--- src/test/java/com/rabo/tppapi/TestUtils/TestUtils.java
+++ src/test/java/com/rabo/tppapi/TestUtils/TestUtils.java
@@ -2,7 +2,6 @@ package com.rabo.tppapi.TestUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -10,62 +9,90 @@ import org.springframework.util.ResourceUtils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-
+/**
+ * Utility Class for tests
+ * @author Nishchal
+ *
+ */
 public class TestUtils {
-	
-	  private TestUtils() throws InstantiationException {
-	        throw new InstantiationException("Cannot instantiate the class");
-	    }
-	    /**
-	     * Reads file and returns {@link InputStream} instance.
-	     *
-	     * @param fileName name of the file
-	     * @return {@code InputStream} instance
-	     */
-	    public static InputStream readFile(final String fileName) {
-	       // final ClassLoader classLoader = getClass().getClassLoader();
-	        return Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName);
-	        //return classLoader.getResourceAsStream(fileName);
-	    }
-	    /*
-	     * converts a Java object into JSON representation
-	     */
-	    public static String asJsonString(final Object obj) {
-	        try {
-	            return new ObjectMapper().writeValueAsString(obj);
-	        } catch (final Exception e) {
-	            throw new RuntimeException(e);
-	        }
-	    }
-	    
-	    public static String readCertificate(String path) throws IOException{
-	    	
-	    	File file = ResourceUtils.getFile(path);
-	    	FileInputStream fis = new FileInputStream(file);
-			byte[] buffer = new byte[65535];
-			StringBuilder sb = new StringBuilder();
-			while (fis.read(buffer) != -1) {
-				sb.append(new String(buffer));
-				buffer = new byte[65535];
-			}
-			fis.close();
 
-			return sb.toString();
-	    }
-	    
-      public static String readSignature(String path) throws IOException{
-	    	
-    	    File file = ResourceUtils.getFile(path);
-	    	FileInputStream fis = new FileInputStream(file);
-			byte[] buffer = new byte[65535];
-			StringBuilder sb = new StringBuilder();
-			while (fis.read(buffer) != -1) {
-				sb.append(new String(buffer));
-				buffer = new byte[65535];
-			}
-			fis.close();
+	private TestUtils() throws InstantiationException {
+		throw new InstantiationException("Cannot instantiate the class");
+	}
 
-			return sb.toString();
-	    }
+	/**
+	 * Reads file and returns {@link InputStream} instance.
+	 *
+	 * @param fileName
+	 *            name of the file
+	 * @return {@code InputStream} instance
+	 */
+	public static InputStream readFile(final String fileName) {
+		return Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName);
+	}
+
+	/**
+	 * converts a Java object into JSON representation
+	 * 
+	 * @param obj
+	 *            input object
+	 * @return string value
+	 */
+	public static String asJsonString(final Object obj) {
+		try {
+			return new ObjectMapper().writeValueAsString(obj);
+		} catch (final Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	/**
+	 * Reads the certificate from the given path
+	 * 
+	 * @param path
+	 *            path of the file
+	 * @return string value
+	 * @throws IOException
+	 *             Exception
+	 */
+	public static String readCertificate(String path) throws IOException {
+
+		File file = ResourceUtils.getFile(path);
+		FileInputStream fis = new FileInputStream(file);
+		byte[] buffer = new byte[65535];
+		StringBuilder sb = new StringBuilder();
+		while (fis.read(buffer) != -1) {
+			sb.append(new String(buffer));
+			buffer = new byte[65535];
+		}
+		fis.close();
+
+		return sb.toString();
+	}
+
+	/**
+	 * Reads the signature from the given path
+	 * 
+	 * @param path
+	 *            path of the file
+	 * @return string value
+	 * @throws IOException
+	 *             Exception
+	 */
+
+	public static String readSignature(String path) throws IOException {
+
+		File file = ResourceUtils.getFile(path);
+		FileInputStream fis = new FileInputStream(file);
+		byte[] buffer = new byte[65535];
+		StringBuilder sb = new StringBuilder();
+		while (fis.read(buffer) != -1) {
+			sb.append(new String(buffer));
+			buffer = new byte[65535];
+		}
+		fis.close();
+
+		return sb.toString();
+	}
 
 }
--- src/test/java/com/rabo/tppapi/service/implementation/PaymentInitiationServiceImplTest.java
+++ src/test/java/com/rabo/tppapi/service/implementation/PaymentInitiationServiceImplTest.java
@@ -26,75 +26,68 @@ import com.rabo.tppapi.validator.PaymentInitiationRequestValidator;
 @RunWith(MockitoJUnitRunner.class)
 @ExtendWith(MockitoExtension.class)
 public class PaymentInitiationServiceImplTest {
-	
+
 	@InjectMocks
 	PaymentInitiationServiceImpl service;
-	
+
 	@Mock
-	PaymentInitiationRequestValidator validator; 
-	
+	PaymentInitiationRequestValidator validator;
+
 	@Spy
 	PaymentInitiationResponse response;
-	
+
 	private final ObjectMapper mapper = new ObjectMapper();
-	
-	String encodedCertificate= null;
-	
+
+	String encodedCertificate = null;
+
 	String encodedSignature = null;
-	
+
 	PaymentInitiationRequest request;
-	
-	
+
 	@Test
-	public void givenValidRequest_WhenMethodIsCalled_ThenReturnsSuccessfulResponse() throws IOException{
-		
+	public void givenValidRequest_WhenMethodIsCalled_ThenReturnsSuccessfulResponse() throws IOException {
+
 		perpareDataForSuccesfulResponse();
-		
-		PaymentInitiationResponseOverview responseOverview = service.validateRequest(encodedCertificate.trim(), encodedSignature.trim(), request);
-		
+
+		PaymentInitiationResponseOverview responseOverview = service.validateRequest(encodedCertificate.trim(),
+				encodedSignature.trim(), request);
+
 		assertEquals("Accepted", responseOverview.getResponse().getStatus());
-		
+
 	}
-	
-	@Test(expected=GenericException.class)
-	public void givenInvalidCertificateRequest_WhenMethodIsCalled_ThenReturnsUnSuccessfulResponse() throws IOException{
-		
+
+	@Test(expected = GenericException.class)
+	public void givenInvalidCertificateRequest_WhenMethodIsCalled_ThenReturnsUnSuccessfulResponse() throws IOException {
+
 		perpareDataForInvalidCertificate();
-		
-		PaymentInitiationResponseOverview responseOverview = service.validateRequest(encodedCertificate.trim(), encodedSignature.trim(), request);
-		
+
+		PaymentInitiationResponseOverview responseOverview = service.validateRequest(encodedCertificate.trim(),
+				encodedSignature.trim(), request);
+
 		assertEquals("Rejected", responseOverview.getResponse().getStatus());
-		
-	}
-	
 
-	
-	
-	private void perpareDataForSuccesfulResponse() throws IOException{
-		
-		String certFilePath = "classpath:ValidCertificate.txt"; 
-    	String signFilePath = "classpath:ValidRequestSignature.txt"; 
-    	encodedCertificate =  TestUtils.readCertificate(certFilePath.trim());
-    	encodedSignature = TestUtils.readSignature(signFilePath.trim());
-		request = mapper.readValue(TestUtils.readFile("test-valid-request.json"), PaymentInitiationRequest.class);
-		Mockito.doNothing().when(validator).validateRequest(Mockito.anyObject(), Mockito.anyString(), Mockito.anyObject());
-		
 	}
-	
-    private void perpareDataForInvalidCertificate() throws IOException{
-		
-		String certFilePath = "classpath:InvalidCertificate.txt"; 
-    	String signFilePath = "classpath:ValidRequestSignature.txt"; 
-    	encodedCertificate =  TestUtils.readCertificate(certFilePath.trim());
-    	encodedSignature = TestUtils.readSignature(signFilePath.trim());
-		request = mapper.readValue(TestUtils.readFile("test-valid-request.json"), PaymentInitiationRequest.class);
-		
-	}
-    
 
-    
-    
-	
-	
+	private void perpareDataForSuccesfulResponse() throws IOException {
+
+		String certFilePath = "classpath:ValidCertificate.txt";
+		String signFilePath = "classpath:ValidRequestSignature.txt";
+		encodedCertificate = TestUtils.readCertificate(certFilePath.trim());
+		encodedSignature = TestUtils.readSignature(signFilePath.trim());
+		request = mapper.readValue(TestUtils.readFile("test-valid-request.json"), PaymentInitiationRequest.class);
+		Mockito.doNothing().when(validator).validateRequest(Mockito.anyObject(), Mockito.anyString(),
+				Mockito.anyObject());
+
+	}
+
+	private void perpareDataForInvalidCertificate() throws IOException {
+
+		String certFilePath = "classpath:InvalidCertificate.txt";
+		String signFilePath = "classpath:ValidRequestSignature.txt";
+		encodedCertificate = TestUtils.readCertificate(certFilePath.trim());
+		encodedSignature = TestUtils.readSignature(signFilePath.trim());
+		request = mapper.readValue(TestUtils.readFile("test-valid-request.json"), PaymentInitiationRequest.class);
+
+	}
 
 }
